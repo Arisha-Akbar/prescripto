78409447a730f06c07077c5916870ef0e6396560
Arisha-Akbar <arishaakbar713@gmail.com>
2026-04-17 12:50:51 +0500

    Added TopDoctors component and implement ed AppContext

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
     >
       {/*-----------Left Side----------*/}
       <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:-mb-7.5">
-        <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight"> 
+        <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
           Book Appointment <br />
           With Trusted Doctors
         </p>
@@ -32,7 +32,7 @@ const Header = () => {
           href="#speciality"
           className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"
         >
-          Book appointment{" "}
+          Book appointment
           <img className="w-3" src={assets.arrow_icon} alt="" />
         </a>
       </div>
