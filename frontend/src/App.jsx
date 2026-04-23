913e087c0fd0aa5d5ec7da75da7bfd023a64c526
Arisha-Akbar <arishaakbar713@gmail.com>
2026-04-23 13:36:24 +0500

    feat: added doctor listing functionality; implemented API endpoint and integrated with frontend context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,11 +10,13 @@ import MyAppointments from "../pages/MyAppointments";
 import Appointment from "../pages/Appointment";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
-      <Navbar/>
+      <ToastContainer />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/doctors" element={<Doctors />} />
@@ -26,7 +28,7 @@ const App = () => {
         <Route path="/my-appointments" element={<MyAppointments />} />
         <Route path="/appointment/:docId" element={<Appointment />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
