a3d71b10dc578a88f0f36eb4e72439eebe3d500e
Arisha-Akbar <arishaakbar713@gmail.com>
2026-04-23 23:06:02 +0500

    implement registration and login logic

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -10,11 +10,9 @@ const AppContextProvider = (props) => {
   const currencySymbol = "$";
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [doctors, setDoctors] = useState([]);
-
-  const value = {
-    doctors,
-    currencySymbol,
-  };
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : false,
+  );
 
   const getDoctorsData = async () => {
     try {
@@ -30,9 +28,18 @@ const AppContextProvider = (props) => {
     }
   };
 
+  const value = {
+    doctors,
+    currencySymbol,
+    token,
+    setToken,
+    backendUrl,
+  };
+
   useEffect(() => {
     getDoctorsData();
   }, []);
+
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
