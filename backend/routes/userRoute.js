9c04fbd759e07164c75dc560ca546289e2b9ccc0
Arisha-Akbar <arishaakbar713@gmail.com>
2026-04-24 14:04:49 +0500

    feat: added update profile functionality in backend

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,8 +4,10 @@ import {
   registerUser,
   loginUser,
   getProfile,
+  updateProfile,
 } from "../controllers/userController.js";
 import authUser from "../middlewares/authUser.js";
+import upload from "../middlewares/multer.js";
 
 const userRouter = express.Router();
 
@@ -13,5 +15,11 @@ userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 
 userRouter.get("/get-profile", authUser, getProfile);
+userRouter.post(
+  "/update-profile",
+  upload.single("image"),
+  authUser,
+  updateProfile,
+);
 
 export default userRouter;
