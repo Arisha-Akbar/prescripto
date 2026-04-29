205c5dad7091515066e14f6728d0bed7322cefba
Arisha-Akbar <arishaakbar713@gmail.com>
2026-04-29 23:26:22 +0500

    feat: update backend URL handling and restructure Vercel configuration

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -3,8 +3,7 @@ import { createContext } from "react";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-
-const currency = '$'
+  const currency = "$";
 
   const calculateAge = (dob) => {
     const today = new Date();
@@ -35,7 +34,7 @@ const currency = '$'
       dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
     );
   };
-  const value = { calculateAge, slotDateFormat,currency };
+  const value = { calculateAge, slotDateFormat, currency };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
